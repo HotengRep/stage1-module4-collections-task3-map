c85cf3756c2fbe0999fedde2cf1f7bfca7b10c20
Makar <hoteng789@gmail.com>
2023-08-19 18:08:19 +0400

    Work with Map

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> swapMap = new LinkedHashMap<>();
+        SortedMap<Integer,String> sortedMap = new TreeMap<>(sourceMap);
+        for(Map.Entry<Integer,String> entry: sortedMap.entrySet())
+        {
+            swapMap.putIfAbsent(entry.getValue(),entry.getKey());
+        }
+
+        return  swapMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String,Integer> counterString = new HashMap<>();
+        StringTokenizer tokenizer = new StringTokenizer(sentence.toLowerCase(Locale.ROOT).replaceAll
+                (",","").replaceAll("\\.","")," ");
+        while (tokenizer.hasMoreTokens())
+        {
+               String temp = tokenizer.nextToken();
+               int count = counterString.containsKey(temp) ? counterString.get(temp) + 1: 1;
+               counterString.put(temp,count);
+
+        }
+
+        return  counterString;
     }
 }
